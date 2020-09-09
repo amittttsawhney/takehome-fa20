f9852722059cb23c8e21ffb2f3c692383d2bd71a
Amit Sawhney <amit.sawhney2002@gmail.com>
2020-09-09 01:32:00 -0500

    feat: addRestaurant in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,9 +11,24 @@ class App extends Component {
         {id: 1, name: "Golden Harbor", rating: 10},
         {id: 2, name: "Potbelly", rating: 6},
         {id: 3, name: "Noodles and Company", rating: 8},
-      ]
+      ], 
+      addRestaurant: ''
     }
   }
+
+  addRestaurant = () => {
+    var nextId = this.state.restaurants[this.state.restaurants.length - 1].id + 1;
+    var name = document.getElementById("new-restaurant").value;
+    var rating = 0;
+
+    var restaurant = {id: nextId, name: name, rating: rating};
+
+    this.setState({restaurants: this.state.restaurants.concat(restaurant)});
+  }
+
+  handleRestaurantFieldChange = (e) => {
+    this.setState({addRestaurant: e.target.value});
+  } 
 
   render() {
     return (
@@ -22,6 +37,8 @@ class App extends Component {
         {this.state.restaurants.map(x => (
           <Restaurant id={x.id} name={x.name} rating={x.rating} />
         ))}
+        <input value = {this.state.addRestaurant} onChange = {this.handleRestaurantFieldChange} id = "new-restaurant" />
+        <button onClick={this.addRestaurant} type = "button">Add new Restaurant</button>
       </div>
     )
   }
